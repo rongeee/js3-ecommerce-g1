b577eaa7d3a4a0f4d7ea2751dff83562d7087350
Andreas <Hellberg.andreas@gmail.com>
2020-09-01 12:38:03 +0200

    ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React from "react"
+import styled from "styled-components"
 
-export default function ProductCard() {
+const Container = styled.div`
+  border-radius: 7px;
+  width: 400px;
+  margin-bottom: 1em;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 10px 10px 16px -15px rgba(0, 0, 0, 0.58);
+`
+
+const TextWrapper = styled.div``
+const Header = styled.h2``
+const ImageWrapper = styled.div`
+  display: flex;
+`
+
+const Description = styled.p``
+
+export default function ProductCard({
+  description,
+  id,
+  images,
+  name,
+  price,
+  rating,
+  stock,
+}) {
+  console.log(images)
   return (
-    <div>
-      <p>hej</p>
-    </div>
-  );
+    <Container>
+      <ImageWrapper>
+        <img alt="" src={images[0].src.small}></img>
+      </ImageWrapper>
+      <TextWrapper>
+        <Header>{name}</Header>
+        <Description>{description}</Description>
+      </TextWrapper>
+    </Container>
+  )
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,41 +1,41 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import ProductCard from "./ProductCard";
+import React from "react"
+import { useState, useEffect } from "react"
+import ProductCard from "./ProductCard"
 
 const ProductList = () => {
-  let [products, setProducts] = useState({});
+  let [products, setProducts] = useState({})
 
   const PRODUCTS_URL =
-    "https://mock-data-api.firebaseio.com/e-commerce/products.json";
+    "https://mock-data-api.firebaseio.com/e-commerce/products.json"
 
   const getProducts = () => {
-    const url = PRODUCTS_URL;
+    const url = PRODUCTS_URL
 
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        setProducts(data);
-      });
-  };
+        setProducts(data)
+      })
+  }
 
   const renderProductCard = () => {
     return Object.keys(products).map((key) => {
-      const product = products[key];
+      const product = products[key]
 
-      return <ProductCard />;
-    });
-  };
+      return <ProductCard key={key} {...product} />
+    })
+  }
 
   useEffect(() => {
-    getProducts();
-  }, []);
+    getProducts()
+  }, [])
 
   return (
     <div>
       <h2>ProductList</h2>
       {renderProductCard()}
     </div>
-  );
-};
+  )
+}
 
-export default ProductList;
+export default ProductList
