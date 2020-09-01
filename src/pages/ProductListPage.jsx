f3be56ffdc39078099596f55b5f610b576f17d30
Janis Basis Basovs PC <janis_basis@hotmail.com>
2020-09-01 12:33:00 +0300

    added ProductListPage

---
+++ src/pages/ProductListPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductList from "../components/ProductList";
+
+const ProductListPage = () => {
+  return (
+    <div>
+      <h2>ProductListPage</h2>
+      <ProductList />
+    </div>
+  );
+};
+
+export default ProductListPage;
