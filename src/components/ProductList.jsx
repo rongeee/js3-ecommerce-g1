abdedcc0fbc8c617cc9e796a0075acb692a90038
Andreas <Hellberg.andreas@gmail.com>
2020-09-01 11:34:14 +0200

    branch test

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,32 +1,33 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React from "react"
+import { useState, useEffect } from "react"
 
 const ProductList = () => {
-  let [products, setProducts] = useState({});
+  console.log("test")
+  let [products, setProducts] = useState({})
 
   const PRODUCTS_URL =
-    "https://mock-data-api.firebaseio.com/e-commerce/products.json";
+    "https://mock-data-api.firebaseio.com/e-commerce/products.json"
 
   const getProducts = () => {
-    const url = PRODUCTS_URL;
+    const url = PRODUCTS_URL
 
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        setProducts(data);
-      });
-  };
+        console.log(data)
+        setProducts(data)
+      })
+  }
 
   useEffect(() => {
-    getProducts();
-  }, []);
+    getProducts()
+  }, [])
 
   return (
     <div>
       <h2>ProductList</h2>
     </div>
-  );
-};
+  )
+}
 
-export default ProductList;
+export default ProductList
