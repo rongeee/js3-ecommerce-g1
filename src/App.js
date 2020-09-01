a04373ddc046789b351ce98f01997a64ef5e9597
Andreas <Hellberg.andreas@gmail.com>
2020-09-01 16:03:44 +0200

    Cart, implemented cart-context

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
 import ProductList from "./components/ProductList"
 import { Route, Switch } from "react-router-dom"
 import ProductDetailsPage from "./pages/ProductDetailsPage"
 import LayoutDefault from "./components/LayoutDefault"
+import { CartContext } from "./context/CartContext"
 
 function App() {
+  const [cart, setCart] = useState({})
   return (
     <div>
       <Switch>
-        <Route
-          path="/product-details/:id"
-          render={(props) => {
-            return (
-              <LayoutDefault>
-                <ProductDetailsPage {...props} />
-              </LayoutDefault>
-            )
-          }}
-        ></Route>
+        <CartContext.Provider value={{ cart, setCart }}>
+          <Route
+            path="/product-details/:id"
+            render={(props) => {
+              return (
+                <LayoutDefault>
+                  <ProductDetailsPage {...props} />
+                </LayoutDefault>
+              )
+            }}
+          ></Route>
 
-        <Route path="/">
-          <LayoutDefault>
-            <ProductList />
-          </LayoutDefault>
-        </Route>
+          <Route path="/">
+            <LayoutDefault>
+              <ProductList />
+            </LayoutDefault>
+          </Route>
+        </CartContext.Provider>
       </Switch>
     </div>
   )
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react"
+import { CartContext } from "../context/CartContext"
 
 export default function Cart() {
-  return <div></div>;
+  const [isHidden, setIsHidden] = useState(false)
+
+  const cart = useContext(CartContext)
+
+  useEffect(() => {
+    console.log("cart updated")
+  }, [cart.cart])
+
+  const handleClick = () => {
+    setIsHidden(!isHidden)
+    cart.setCart({ value: "newcart" })
+  }
+  return <div onClick={handleClick}>I am the cart</div>
 }
