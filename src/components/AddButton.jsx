c34c231ebf87636851823ca2eb379e1b629acb41
Andreas <Hellberg.andreas@gmail.com>
2020-09-03 16:16:28 +0200

    cleanup

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,52 +1,29 @@
-import React, { useContext } from "react";
-import styled from "styled-components";
-import { CartContext } from "../context/CartContext";
-import { addQty, checkInStock } from "../cart.utils";
+import React, { useContext } from "react"
+import styled from "styled-components"
+import { CartContext } from "../context/CartContext"
+import { addQty } from "../cart.utils"
 
 export const AddButton = ({ myProps }) => {
-  const { cart, setCart } = useContext(CartContext);
+  const { cart, setCart } = useContext(CartContext)
 
-  const { id, name, price, stock, images } = myProps;
-
-  const setLocalStorage = () => {};
-
-  const addQty = () => {
-    if (checkInStock()) {
-      setCart(prevState => {
-        return {
-          ...prevState,
-          [id]: { ...prevState[id], qty: prevState[id].qty++ },
-        };
-      });
-    }
-  };
-
-  const checkInStock = () => {
-    console.log(cart[id].qty);
-    if (cart[id].qty <= stock) {
-      return true;
-    } else {
-      console.log("TOO MANY");
-      return false;
-    }
-  };
+  const { id, name, price, stock, images } = myProps
 
   const handleClick = () => {
-    const product = { [id]: { name, price, qty: 1, images } };
+    const product = { [id]: { name, price, qty: 1, images } }
 
     if (cart[id]) {
-      addQty(cart, setCart, id, stock, 1);
+      addQty(cart, setCart, id, stock, 1)
     } else {
-      setCart(prevState => {
+      setCart((prevState) => {
         return {
           ...prevState,
           [id]: { ...product[id], id, stock },
-        };
-      });
+        }
+      })
     }
-  };
-  return <Btn onClick={handleClick}>Add to cart</Btn>;
-};
+  }
+  return <Btn onClick={handleClick}>Add to cart</Btn>
+}
 
 const Btn = styled.button`
   background: #0f0f6d;
@@ -65,4 +42,4 @@ const Btn = styled.button`
     border-radius: 10px;
     box-shadow: 0px 3px 7px #0000ff61;
   }
-`;
+`
