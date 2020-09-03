a4861d68c57f7491b9512bed5ff688c0897c5df3
Patric Ronge <pronge@gmail.com>
2020-09-03 14:49:21 +0200

    remove product if qty < 1 in cart

--- src/components/CartQtyBtn.jsx
+++ src/components/CartQtyBtn.jsx
@@ -1,34 +1,34 @@
-import React, { useContext } from "react"
-import styled from "styled-components"
-import { addQty, deleteProduct } from "../cart.utils"
-import { CartContext } from "../context/CartContext"
-import { resolveMotionValue } from "framer-motion"
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { addQty, deleteProduct } from "../cart.utils";
+import { CartContext } from "../context/CartContext";
+import { resolveMotionValue } from "framer-motion";
 
 const CartQtyBtn = ({ action, product, value }) => {
-  const { cart, setCart } = useContext(CartContext)
+  const { cart, setCart } = useContext(CartContext);
+
+  const handleClick = () => {
+    if (product.qty === 1 && value === -1) {
+      deleteProduct(setCart, product.id);
+    } else {
+      addQty(cart, setCart, product.id, product.stock, value);
+    }
+  };
 
   return (
     <Container>
-      <Button
-        onClick={
-          product.qty === 1
-            ? () => deleteProduct(setCart, product.id)
-            : () => addQty(cart, setCart, product.id, product.stock, value)
-        }
-      >
-        {action}
-      </Button>
+      <Button onClick={handleClick}>{action}</Button>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
   margin: 8px 0px;
-`
+`;
 const Button = styled.button`
   border-radius: 50%;
   height: 20px;
   width: 20px;
-`
+`;
 
-export default CartQtyBtn
+export default CartQtyBtn;
