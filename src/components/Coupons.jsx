d556060185767368c12f157dc0120ec1b78d7d74
Patric Ronge <pronge@gmail.com>
2020-09-03 14:03:22 +0200

    added coupon msg

--- src/components/Coupons.jsx
+++ src/components/Coupons.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState, useRef, useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Coupons = ({ setDiscountPrice, totalPrice }) => {
   const [coupons, setCoupons] = useState({});
+  let [couponMsg, setCouponMsg] = useState(null);
   const { cart } = useContext(CartContext);
   const url =
     "https://mock-data-api.firebaseio.com/e-commerce/couponCodes.json";
@@ -27,21 +29,37 @@ const Coupons = ({ setDiscountPrice, totalPrice }) => {
 
     couponsArr.find((coupon) => {
       console.log(coupons[coupon]);
-      if (coupon === couponCode.current.value && coupons[coupon].valid) {
-        return giveDiscount(couponCode.current.value);
+
+      if (coupon === couponCode.current.value) {
+        if (coupons[coupon].valid) {
+          setCouponMsg("Code applied successfully!");
+          return giveDiscount(couponCode.current.value);
+        } else {
+          setCouponMsg("This code has expired.");
+        }
+      } else {
+        setCouponMsg("This code does not exist.");
       }
     });
   };
 
   const giveDiscount = (currentCoupon) => {
     let tempPrice = Math.round(totalPrice * coupons[currentCoupon].discount);
+
     setDiscountPrice(tempPrice);
   };
 
-  // console.log(coupons);
-
   return (
     <div>
+      {couponMsg && (
+        <motion.p
+          animate={{ x: 100, opacity: 1 }}
+          initial={{ opacity: 0 }}
+          transition={{ x: 100, opacity: 1 }}
+        >
+          {couponMsg}
+        </motion.p>
+      )}
       <input type="text" ref={couponCode} />
       <button onClick={checkCoupons}>Check</button>
     </div>
